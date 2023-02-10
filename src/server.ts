8494b4cb348cf757a1f7443d5b44336b1f7e05bc
JLossavaro <jclossavaro10@gmail.com>
2023-02-10 16:53:45 -0300

    feature/criando-server

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,30 @@
 import express from 'express'
+import { EventsService } from './services';
 
 import { Router, Request, Response } from 'express';
 
 const app = express();
-
 const route = Router()
-
 app.use(express.json())
+app.listen(3333, () => 'server running on port 3333')
+app.use(route)
+
+const eventsService = new EventsService();
+
+
 
 route.get('/', (req: Request, res: Response) => {
   res.json({ message: 'hello world with Typescript' })
 })
 
-app.use(route)
+route.get('/events', (req: Request, res: Response) => {
+  eventsService.getEvents() ;
+});
 
 
-app.listen(3333, () => 'server running on port 3333')
+
+
+
+
+
+

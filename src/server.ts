1bb8b2c73f22a4a3c8681439d34f18f8976a759a
JLossavaro <jclossavaro10@gmail.com>
2023-02-11 15:43:19 -0300

    feature/adicionando-tsx

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express from 'express'
-import { EventsService } from './services';
 
 import { Router, Request, Response } from 'express';
 
@@ -7,21 +6,20 @@ const app = express();
 const route = Router()
 app.use(express.json())
 app.listen(3333, () => 'server running on port 3333')
-app.use(route)
 
-const eventsService = new EventsService();
 
 
 
 route.get('/', (req: Request, res: Response) => {
+  console.log("teste")
   res.json({ message: 'hello world with Typescript' })
 })
 
 route.get('/events', (req: Request, res: Response) => {
-  eventsService.getEvents() ;
+ //todo: implement
 });
 
-
+app.use(route)
 
 
 
